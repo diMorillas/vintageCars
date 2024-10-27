27619f475a626465cda5252d44b0717d54d40e8e
diMorillas <didacmorillas19@gmail.com>
2024-10-27 04:46:42 +0100

    Comented code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-window.onload = ()=>{
-    const buscadorCoches =()=>{
-        const valorBuscador = document.getElementById('searchInput');
-        const coches = document.querySelectorAll('.card');
-        
-        searchInput.addEventListener('input', function() { 
+window.onload = () => {
+    const buscadorCoches = () => {
+        const valorBuscador = document.getElementById('cocheEscrito'); //Es el texto que ponemos en el buscador.
+        const coches = document.querySelectorAll('.card'); // Todas las cartas de los coches, es el div que contiene el resto de elementos.
+
+        valorBuscador.addEventListener('input', function () {
             const buscarCoche = valorBuscador.value.toLowerCase();
-        
-            coches.forEach(function(card) {
+
+            coches.forEach(function (card) {
                 const nomberCoche = card.querySelector('.car-title').textContent.toLowerCase();
-                
-                if (nomberCoche.includes(buscarCoche)) {
+
+                if (nomberCoche.includes(buscarCoche)) { //Si existe un string como el de nombreCoche que hemos dicho que es el nombre del coche anteriormente.
                     card.style.display = 'block';
                 } else {
                     card.style.display = 'none';
                 }
             });
-        });  
+        });
     }
-  
+
     buscadorCoches();
 }
